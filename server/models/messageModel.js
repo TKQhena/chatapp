7ffb7f5cb6e9f08d5006d4fcb89729eb2a2b7068
GhostlyTK <tokelojr@gmail.com>
2023-08-07 14:48:17 +0200

    models

---
+++ server/models/messageModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    recipient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    text: {
+        type: String
+    }
+},{
+    timestamps: true
+})
+
+const Message = mongoose.model('Message', MessageSchema);
+module.exports = Message;
